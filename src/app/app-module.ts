f3238470ba985fa3a2862505d3d8f47a8267f065
puja lakhe <lakhepuza924@gmail.com>
2025-09-24 16:16:18 +0545

    add httpClientModule provider and also ngrx roots

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideStoreDevtools()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot(),
+    EffectsModule.forRoot(),
+  ],
+  providers: [provideStoreDevtools(), provideHttpClient()],
   bootstrap: [App],
 })
 export class AppModule {}
