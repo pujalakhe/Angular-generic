d1771aaf0a1ea795d1583cf6a429df7892e06892
puja lakhe <lakhepuza924@gmail.com>
2025-09-24 16:12:38 +0545

    refactor: streamline app module and enhance theming with Angular Material

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,20 +1,14 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideStoreDevtools()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
