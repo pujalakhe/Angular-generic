d11338cd91ebd69a20ad520c9535f1b102e2d597
puja lakhe <lakhepuza924@gmail.com>
2025-09-25 14:15:45 +0545

    employee form component

--- src/app/feature/private/employee-module/employee-module.ts
+++ src/app/feature/private/employee-module/employee-module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { EmployeeRoutingModule } from './employee-routing-module';
 import { PrivateLayoutComponent } from '../../../core/constants/components/private-layout-component/private-layout-component';
 import { EmployeeListComponent } from './components/employee-list-component/employee-list-component';
+import { EmployeeFormComponent } from './components/employee-form-component/employee-form-component';
 
 @NgModule({
   declarations: [
-    EmployeeListComponent
+    EmployeeListComponent,
+    EmployeeFormComponent
   ],
   imports: [CommonModule, EmployeeRoutingModule, PrivateLayoutComponent],
 })
