9723501800198e075770fa9487fb0e17f5cea566
puja lakhe <lakhepuza924@gmail.com>
2025-09-25 14:15:36 +0545

    form component

---
+++ src/app/feature/private/employee-module/components/employee-form-component/employee-form-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-form-component',
+  standalone: false,
+  templateUrl: './employee-form-component.html',
+  styleUrl: './employee-form-component.scss'
+})
+export class EmployeeFormComponent {
+
+}
