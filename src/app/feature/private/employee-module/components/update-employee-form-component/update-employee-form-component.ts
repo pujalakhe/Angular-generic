ecc9819c9595af25a67a72a52f125ae41666318d
puja lakhe <lakhepuza924@gmail.com>
2025-09-25 15:28:22 +0545

    add update button functionality

--- src/app/feature/private/employee-module/components/update-employee-form-component/update-employee-form-component.ts
+++ src/app/feature/private/employee-module/components/update-employee-form-component/update-employee-form-component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Employee } from '../../model/employee-model';
 
 @Component({
   selector: 'app-update-employee-form-component',
   standalone: false,
   templateUrl: './update-employee-form-component.html',
-  styleUrl: './update-employee-form-component.scss'
+  styleUrl: './update-employee-form-component.scss',
 })
 export class UpdateEmployeeFormComponent {
-
+  updateEmployee(employee: Employee): void {
+    console.log(employee);
+  }
 }
