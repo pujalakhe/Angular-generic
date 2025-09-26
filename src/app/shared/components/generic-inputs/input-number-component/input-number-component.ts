b51f06453c58ee1bf14f51d42a4dd3809646c35b
puja lakhe <lakhepuza924@gmail.com>
2025-09-26 13:43:32 +0545

    input number component

--- src/app/shared/components/generic-inputs/input-number-component/input-number-component.ts
+++ src/app/shared/components/generic-inputs/input-number-component/input-number-component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MaterialModule } from '../../../material/material-module';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { OnlyNumericValueDirective } from '../../../custom-directives/only-numeric-value-directive';
 
 @Component({
   selector: 'app-input-number-component',
-  imports: [],
+  imports: [MaterialModule, ReactiveFormsModule, OnlyNumericValueDirective],
   templateUrl: './input-number-component.html',
-  styleUrl: './input-number-component.scss'
+  styleUrl: './input-number-component.scss',
 })
 export class InputNumberComponent {
-
+  @Input() control?: FormControl;
+  @Input() label?: string;
+  @Input() placeholder: string = '';
+  @Input() min?: number;
 }
