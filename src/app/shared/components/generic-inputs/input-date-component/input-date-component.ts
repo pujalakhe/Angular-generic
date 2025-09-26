1f16e2e1d27f79e7f67080f2061da79e1f7e044f
puja lakhe <lakhepuza924@gmail.com>
2025-09-26 13:40:18 +0545

    generic input date component

--- src/app/shared/components/generic-inputs/input-date-component/input-date-component.ts
+++ src/app/shared/components/generic-inputs/input-date-component/input-date-component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MaterialModule } from '../../../material/material-module';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-date-component',
-  imports: [],
+  imports: [MaterialModule, ReactiveFormsModule],
   templateUrl: './input-date-component.html',
-  styleUrl: './input-date-component.scss'
+  styleUrl: './input-date-component.scss',
 })
 export class InputDateComponent {
-
+  @Input({ required: true }) control?: FormControl;
+  @Input({ required: true }) label?: string;
+  @Input({ required: true }) placeholder: string = '';
 }
