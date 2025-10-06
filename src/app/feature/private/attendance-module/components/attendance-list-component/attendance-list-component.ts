12c33eb54446b41cf6b5fe6c4b8d0dc741d96856
puja lakhe <lakhepuza924@gmail.com>
2025-10-06 10:51:05 +0545

    attendance list component

---
+++ src/app/feature/private/attendance-module/components/attendance-list-component/attendance-list-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance-list-component',
+  standalone: false,
+  templateUrl: './attendance-list-component.html',
+  styleUrl: './attendance-list-component.scss'
+})
+export class AttendanceListComponent {
+
+}
