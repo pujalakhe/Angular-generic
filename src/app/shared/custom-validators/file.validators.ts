a459b5bc88c6c3bcfa7118d2ad9cc67a3b5f6495
puja lakhe <lakhepuza924@gmail.com>
2025-10-08 14:03:39 +0545

    custom file validator

--- src/app/shared/custom-validators/file.validators.ts
+++ src/app/shared/custom-validators/file.validators.ts
@@ -5,8 +5,8 @@ export function fileValidator(
   allowedTypes: string[]
 ): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    const file = control.value;
-    if (!file) return null; // no file, valid
+    const file = control.value as File | null;
+    if (!file) return null;
 
     // Check file size
     if (file.size > maxSize) {
