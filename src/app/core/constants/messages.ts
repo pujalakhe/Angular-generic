56eddc756191aa2205a2ac0df3427ffdfd498ac4
puja lakhe <lakhepuza924@gmail.com>
2025-10-08 14:01:45 +0545

    write the comment

--- src/app/core/constants/messages.ts
+++ src/app/core/constants/messages.ts
@@ -1,3 +1,8 @@
 export const UNSAVED_CHANGES_MESSAGE =
   'You have unsaved changes. Do you want to leave?';
-export const MAT_DIALOG_WIDTH = '400px';
+
+export const MAT_DIALOG_WIDTH = '400px'; //width of mat dialog
+
+export const MAX_IMAGE_SIZE = 3145728; //size in bytes. 3145728 bytes = 3MB
+
+export const IMAGE_FILE_TYPE = 'image/png';
