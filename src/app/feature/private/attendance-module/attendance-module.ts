37f1d487cccc0cd4ec3db738220d4b25566710d6
puja lakhe <lakhepuza924@gmail.com>
2025-10-09 11:26:54 +0545

    import reactive form modules

--- src/app/feature/private/attendance-module/attendance-module.ts
+++ src/app/feature/private/attendance-module/attendance-module.ts
@@ -7,15 +7,18 @@ import { AttendanceListComponent } from './components/attendance-list-component/
 import { FilterComponent } from '../../../shared/components/filter-component/filter-component';
 
 import { FileUploadComponent } from '../../../shared/components/file-upload-component/file-upload-component';
+import { FormArrayComponent } from './components/form-array-component/form-array-component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AttendanceListComponent],
+  declarations: [AttendanceListComponent, FormArrayComponent],
   imports: [
     CommonModule,
     AttendanceRoutingModule,
     PrivateLayoutComponent,
     FilterComponent,
     FileUploadComponent,
+    ReactiveFormsModule,
   ],
 })
 export class AttendanceModule {}
